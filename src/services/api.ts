84ec27b946655674480fd92871a6491e548ba439
LucasPaszinski <lucaspaszinski@gmail.com>
2020-08-09 19:51:00 -0300

    Api ip updated.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
-import Axios from 'axios'
 
-const api = Axios.create({ baseURL: "http://10.0.0.111:3333" })
+const api = axios.create({ baseURL: "http://192.168.25.6:3333" })
 
 export default api
