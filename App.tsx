71e794cc97620fa3aa6bb704bdc04ce429fd873a
lucas.paszinski <lucas.paszinski@altus.com.br>
2020-08-08 21:45:28 -0300

    While fonts are not loaded, keep showing the splash screen.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
 import Landing from './src/pages/Landing'
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo'
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
+
+
+import { AppLoading } from 'expo'
 
 // <> </> is a fragment, does not create extra html 
 
 export default function App()
 {
-  return (
-    <>
-      <StatusBar style="auto" />
-      <Landing />
-    </>
-  );
+
+  let [ fontsLoaded ] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  })
+
+  if (!fontsLoaded)
+  {
+    return <AppLoading />
+  }
+  else
+  {
+    return (
+      <>
+        <StatusBar style='light' />
+        <Landing />
+      </>
+    )
+  }
+
 }
