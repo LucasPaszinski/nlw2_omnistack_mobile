ca8cde9f1be312eb9e31016f4974410af6fcc8e3
lucas.paszinski <lucas.paszinski@altus.com.br>
2020-08-08 23:47:18 -0300

    Finished Give Class component

--- src/pages/GiveClass/index.tsx
+++ src/pages/GiveClass/index.tsx
@@ -1,15 +1,39 @@
 import React, { useState } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground } from 'react-native'
 import styles from './styles'
 
+import { RectButton } from 'react-native-gesture-handler'
+
+import giveClassesBackgroundImage from '../../assets/images/give-classes-background.png'
+
+import { useNavigation } from '@react-navigation/native'
 
 function GiveClass()
 {
+    const { navigate } = useNavigation();
+
     return (
-        <View>
-            <Text>
-                GiveClass
-            </Text>
+        <View style={styles.container}>
+            <ImageBackground
+                source={giveClassesBackgroundImage}
+                style={styles.content}
+                resizeMode='contain'
+            >
+                <Text style={styles.title}>
+                    Quer ser um proffy?
+                </Text>
+                <Text style={styles.description}>
+                    Para começar, você precisa se cadastrar como professor em nossa plataforma web.
+                </Text>
+            </ImageBackground>
+            <RectButton
+                style={styles.button}
+                onPress={() => navigate('Landing')}
+            >
+                <Text style={styles.buttonText}>
+                    Tudo Bem
+                </Text>
+            </RectButton>
         </View>
 
     )
--- src/pages/GiveClass/styles.ts
+++ src/pages/GiveClass/styles.ts
@@ -1,11 +1,50 @@
 import { StyleSheet } from 'react-native'
 
-// Archivo_400Regular,
-// Archivo_700Bold,
-// Poppins_400Regular,
+// Archivo_400Regular
+// Archivo_700Bold
+// Poppins_400Regular
 // Poppins_600SemiBold
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1, // Occupy all screen
+        backgroundColor: '#8257e5',
+        justifyContent: 'center', // center contents
+        padding: 40, // apply 40 padding all items
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    title: {
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 32,
+        color: "#fff",
+        lineHeight: 37,
+        maxWidth: 180,
+    },
+    description: {
+        marginTop: 24,
+        color: "#d4c2ff",
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: 'Poppins_400Regular',
+        maxWidth: 240,
+    },
+    button: {
+        marginVertical:40,
+        backgroundColor: '#04d361',
+        height: 58,
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: "center"
+    },
+    buttonText: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#FFF',
+        fontSize: 16,
+        justifyContent: "center",
+    },
 })
 
 export default styles;
