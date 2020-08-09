4e4f4afa2add1ae3ea3a5f0d0494caf7ddb862a9
lucas.paszinski <lucas.paszinski@altus.com.br>
2020-08-09 00:51:41 -0300

    Dummy components, template for new components. (DELETE AT END)

---
+++ src/component/BaseComponent/index.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { View, Text } from 'react-native'
+import styles from './styles'
+
+
+function GIVE_NAME_HERE()
+{
+    return (
+        <View>
+            <Text>
+                GIVE_NAME_HERE
+            </Text>
+        </View>
+
+    )
+}
+
+export default GIVE_NAME_HERE;
---
+++ src/component/BaseComponent/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+// Archivo_400Regular,
+// Archivo_700Bold,
+// Poppins_400Regular,
+// Poppins_600SemiBold
+
+const styles = StyleSheet.create({
+})
+
+export default styles;
